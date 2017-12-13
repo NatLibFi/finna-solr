aa2c3672565f9d040fbbc6f66f984a35d54f7769
Ere Maijala <ere.maijala@helsinki.fi>
2017-12-13 16:32:05 +0200

    Add possibility to define Solr port in cluster_status script and refine the results a bit.

--- scripts/cluster_status.php
+++ scripts/cluster_status.php
@@ -1,11 +1,13 @@
 #!/usr/bin/php
 <?php
 
+$port = isset($argv[1]) ? $argv[1] : '8983';
+
 $errors = false;
-$state = `curl -s "http://localhost:8983/solr/admin/collections?action=clusterstatus&wt=json"`;
+$state = `curl -s "http://localhost:$port/solr/admin/collections?action=clusterstatus&wt=json"`;
 
 if (empty($state)) {
-    echo "Could not get Solr status";
+    echo "Could not get Solr status\n";
     exit(1);
 }
 
@@ -13,10 +15,11 @@ $state = json_decode($state, true);
 foreach ($state['cluster']['collections'] as $collectionName => $collection) {
     echo "Collection $collectionName:\n";
     foreach ($collection['shards'] as $shardName => $shard) {
-        echo "  Shard $shardName " . $shard['state'] . ":\n";
+        echo "  Shard $shardName (" . $shard['state'] . "):\n";
         foreach ($shard['replicas'] as $coreNodeName => $replica) {
             echo '    ' . substr($replica['state'] . '         ', 0, 10) . ' ' . $replica['core'] . " ($coreNodeName) at " . $replica['node_name'] . "\n";
         }
-    } 
+    }
+    echo "\n";
 }
 
