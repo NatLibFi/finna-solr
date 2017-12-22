55aff3be34368a88a426fa53d44e9dd440cdd81c
Ere Maijala <ere.maijala@helsinki.fi>
2017-12-22 10:06:07 +0200

    Add a -q option to the cluster status check script to avoid any output unless there are problems.

--- scripts/cluster_status.php
+++ scripts/cluster_status.php
@@ -1,29 +1,52 @@
 #!/usr/bin/php
 <?php
 
-$port = isset($argv[1]) ? $argv[1] : '8983';
+$port = 8983;
+$quiet = false;
+array_shift($argv);
+foreach ($argv as $arg) {
+    if ('-q' === $arg) {
+        $quiet = true;
+    } else {
+        $port = $arg;
+    }
+}
 
-$errors = false;
 $state = `curl -s "http://localhost:$port/solr/admin/collections?action=clusterstatus&wt=json"`;
 
 if (empty($state)) {
-    echo "Could not get Solr status\n";
+    echo "Could not get Solr status, port $port\n";
     exit(1);
 }
 
+$errors = false;
+$results = '';
 $state = json_decode($state, true);
 foreach ($state['cluster']['collections'] as $collectionName => $collection) {
-    echo "Collection $collectionName:\n";
+    $results .= "Collection $collectionName (Solr port $port):\n";
     foreach ($collection['shards'] as $shardName => $shard) {
-        echo "  Shard $shardName (" . $shard['state'] . "):\n";
+        if ('active' !== $shard['state']) {
+            $errors = true;
+        }
+        $results .= "  Shard $shardName (" . $shard['state'] . "):\n";
         foreach ($shard['replicas'] as $coreNodeName => $replica) {
+            if ('active' !== $replica['state']) {
+                $errors = true;
+            }
             $info = "$coreNodeName, {$replica['type']}";
             if (!empty($replica['leader'])) {
                 $info .= ', leader';
             }
-            echo '    ' . substr($replica['state'] . '         ', 0, 10) . ' ' . $replica['core'] . " ($info) at " . $replica['node_name'] . "\n";
+            $results .= '    ' . substr($replica['state'] . '         ', 0, 10) . ' ' . $replica['core'] . " ($info) at " . $replica['node_name'] . "\n";
         }
     }
-    echo "\n";
+    $results .= "\n";
+}
+
+if (!$quiet || $errors) {
+    if ($errors) {
+        echo "WARNING: Solr cluster degraded:\n\n";
+    }
+    echo $results;
 }
 
