ad529df3ab6733ca8ea851955fc2b4061cbaae5f
Ere Maijala <ere.maijala@helsinki.fi>
2018-04-04 10:09:15 +0300

    Check that user exists when creating an instance dir.

--- scripts/create_instance_dir.sh
+++ scripts/create_instance_dir.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -z "$SOLR_USER" ]; then
+  SOLR_USER=solr
+fi
+
 if [[ $# -eq 0 ]]
 then
   echo "Usage: $0 instancedir [instancedir...]"
@@ -17,8 +21,14 @@ do
   cp vufind/solr.xml $DIR/
   mkdir $DIR/logs
   ln -s ../vufind/lib $DIR/lib
-  chown -R solr $DIR
-  
+  ln -s ../vufind/log4j.properties $DIR/
+  if [[ `id -u $SOLR_USER 2>/dev/null || echo -1` -ge 0 ]];
+  then
+    chown -R solr $DIR
+  else
+    echo "WARNING: User '$SOLR_USER' not found, skipping permissions setup"
+  fi
+
   echo "$DIR created"
 done
 
